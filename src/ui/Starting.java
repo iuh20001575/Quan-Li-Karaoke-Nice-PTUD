0de9688cc5ea817d95e29fc64b36b2d86cd60f82
Thao Ha <thaoanhhaa1@gmail.com>
2022-08-23 09:43:12 +0700

    Config Base

---
+++ src/ui/Starting.java
@@ -0,0 +1,16 @@
+package ui;
+
+import javax.swing.SwingUtilities;
+
+public class Starting {
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+//				Login_GUI login_GUI = new Login_GUI();
+//				login_GUI.setVisible(true);
+			}
+		});
+	}
+}
